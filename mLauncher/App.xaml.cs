28a97b16efd413d54a166c85bf5e115131a8661a
motolies <knw1234@gmail.com>
2019-12-23 23:15:37 +0900

    중복실행되면 먼저 실행한 프로세스를 죽이고 새로운 프로세스로 시작한다

--- mLauncher/App.xaml.cs
+++ mLauncher/App.xaml.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +17,37 @@ namespace mLauncher
     /// </summary>
     public partial class App : Application
     {
+        private static string mutexName = Assembly.GetExecutingAssembly().GetName().Name;
+        private static Mutex mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createNew;
+            mutex = new Mutex(true, mutexName, out createNew);
+            if (createNew)
+            {
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                // 이전에 실행한 프로그램이 있다면 닫고 새로 연다
+                List<Process> processes = Process.GetProcessesByName("mLauncher").Where(p => p.HandleCount > 0).ToList();
+                foreach (Process proc in processes)
+                {
+                    if (proc.ProcessName.Equals(mutexName) && proc.Id != Process.GetCurrentProcess().Id)
+                    {
+                        proc.Kill();
+                    }
+                }
+            }
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (mutex != null)
+                mutex.ReleaseMutex();
+            base.OnExit(e);
+        }
     }
 }
