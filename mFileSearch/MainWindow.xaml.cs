17c7cafcdf7fae9671e7aad7c632ba00d5f03c31
motolies <knw1234@gmail.com>
2019-12-27 11:15:48 +0900

    검색은 이제 조금 되는거 같다

--- mFileSearch/MainWindow.xaml.cs
+++ mFileSearch/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using mUT;
+﻿using mEx;
+using mUT;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,8 +28,10 @@ namespace mFileSearch
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        object syncMatchCount = new object();
+        object syncLineNumber = new object();
         public static ThreadWorker tw = new ThreadWorker();
+        private bool ThreadStop = false;
 
         public MainWindow()
         {
@@ -36,14 +42,10 @@ namespace mFileSearch
 
             this.DataContext = this;
 
-
-            //Folders.Add(new TargetFolder() { Path = "true1", Enable = true });
-            //FindingFiles.Add(new FileFound() { File = "a", Line = 1, Text = "aaa" });
+            Folders.Add(new TargetFolder() { Path = @"D:\Git\mLauncher", Enable = true });
 
             InitWorker();
             InitControl();
-            
-            
         }
 
         private void InitWorker()
@@ -69,18 +71,32 @@ namespace mFileSearch
         #region thread 관련 함수
         private void Tw_OnCompleted(object sender, EventArgs e)
         {
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            Dispatcher.Invoke(DispatcherPriority.Send, new Action(delegate
             {
                 this.IsBusy = false;
             }));
         }
         private void Tw_DoWork(object sender, EventArgs e)
         {
-            //SearchFile(sender);
+            IList<TargetFolder> _fol = new List<TargetFolder>();
+            string _con = string.Empty, _fil = string.Empty;
+            bool _sub = true, _reg = false;
+
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                _fol = Folders;
+                _con = cboCondition.Text;
+                _fil = cboFilter.Text;
+                _sub = IsSubFolder;
+                _reg = IsRegex;
+            }));
+
+            SearchFile(_fol, _con, _fil, _sub, _reg);
+
         }
         private void Tw_OnProcessChanged(object sender, ProgressEventArgs e)
         {
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            Dispatcher.Invoke(DispatcherPriority.Render, new Action(delegate
             {
                 ChangePercent(e.Percent);
             }));
@@ -136,21 +152,136 @@ namespace mFileSearch
         }
         public static readonly DependencyProperty PercentProperty = DependencyProperty.Register("Percent", typeof(string), typeof(MainWindow));
 
+        public int MatchedCount
+        {
+            get { return (int)GetValue(MatchedCountProperty); }
+            set { SetValue(MatchedCountProperty, value); }
+        }
+        public static readonly DependencyProperty MatchedCountProperty = DependencyProperty.Register("MatchedCount", typeof(int), typeof(MainWindow));
+
+    
         #endregion
 
 
 
         private void Search_Click(object sender, RoutedEventArgs e)
         {
+            FindingFiles.Clear();
+            MatchedCount = 0;
             this.IsBusy = true;
+            ThreadStop = !this.IsBusy;
             tw.Run();
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
+            
+            ThreadStop = true;
             tw.Stop();
             this.IsBusy = false;
         }
+
+
+        #region 파일검색
+
+        public void SearchFile(IList<TargetFolder> folders, string condition, string fileFilter, bool isSubFolder, bool isRegex)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            List<string> files = new List<string>();
+            string searchTerm = condition.ToLower();
+            string[] filter = fileFilter.Trim().ToLower().Replace("*.*", string.Empty).Replace("*", string.Empty).Split(';');
+
+            foreach (string folder in folders.Where(f => f.Enable == true).Select(f => f.Path).ToList())
+            {
+                SearchOption so = isSubFolder ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                files.AddRange(Directory.GetFiles(folder, "*.*", so));
+            }
+
+            var withoutFiles = files.Where(ext => !ext.EndWith(new List<String>() { ".dll", ".exe", ".db", ".svn-base", ".pdb", ".cache" }));
+            var fileList = withoutFiles.Where(ext => ext.ToLower().EndWith(filter));
+
+            int totalFile = fileList.Count();
+            int curFile = 0;
+            Parallel.ForEach(fileList, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, (file, stateFile) =>
+            {
+                if (ThreadStop)
+                    stateFile.Stop();
+
+                int lineNumber = 1;
+
+                Encoding encoding = FileIO.GetFileEncoding(file);
+                if (encoding == null)
+                    return;
+
+                foreach (string lineString in File.ReadLines(file, encoding))
+                {
+                    if (ThreadStop)
+                        break;
+
+                    if ((isRegex && Regex.IsMatch(lineString, searchTerm, RegexOptions.IgnoreCase)) || lineString.ToLower().Contains(searchTerm))
+                    {
+                        Dispatcher.Invoke(DispatcherPriority.Render, new Action(delegate
+                        {
+                            MatchedCount++;
+                        }));
+
+                        lock (syncLineNumber)
+                        {
+                            //검색 중간중간에 리스트에 뿌린다
+                            Dispatcher.Invoke(DispatcherPriority.Render, new Action(delegate
+                            {
+                                FindingFiles.Add(new FileFound() { File = file, Line = lineNumber, Text = lineString });
+                            }));
+                            
+                        }
+                    }
+                    lineNumber++;
+                }
+                curFile++;
+                if (curFile % 10 == 0)
+                    tw.ReportProgress((float)curFile / (float)totalFile);
+
+            });
+            tw.ReportProgress((float)1);
+            sw.Stop();
+            Console.WriteLine("검색종료 : " + sw.ElapsedMilliseconds);
+        }
+
+
+        #endregion
+
+        #region drag and drop
+
+        private void lvFolder_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
+                e.Effects = System.Windows.DragDropEffects.Copy;
+        }
+        private void lvFolder_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] file = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string str in file)
+                {
+                    FileAttributes attr = File.GetAttributes(str);
+                    string value = string.Empty;
+                    if (attr.HasFlag(FileAttributes.Directory))
+                        value = str;
+                    else
+                        value = System.IO.Path.GetDirectoryName(str);
+
+                    if (Folders.Where(f => f.Path == value).Count() < 1)
+                        Folders.Add(new TargetFolder() { Path = value, Enable = true });
+                }
+            }
+
+        }
+
+        #endregion
+
     }
 
     public class TargetFolder
---
+++ mUT/File.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mUT
+{
+   public class FileIO
+    {
+
+        //인코딩 확인
+        public static Encoding GetFileEncoding(string srcFile)
+        {
+            try
+            {
+                // *** Use Default of Encoding.Default (Ansi CodePage)
+                Encoding enc = Encoding.Default;
+                // *** Detect byte order mark if any - otherwise assume default
+                byte[] buffer = new byte[10];
+                FileStream file = new FileStream(srcFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+                file.Read(buffer, 0, 10);
+                file.Close();
+                if (buffer[0] == 0xef && buffer[1] == 0xbb && buffer[2] == 0xbf)
+                    enc = Encoding.UTF8; //with BOM
+                else if (buffer[0] == 0xfe && buffer[1] == 0xff)
+                    enc = Encoding.Unicode;
+                else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xfe && buffer[3] == 0xff)
+                    enc = Encoding.UTF32;
+                else if (buffer[0] == 0x2b && buffer[1] == 0x2f && buffer[2] == 0x76)
+                    enc = Encoding.UTF7;
+                else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+                    // 1201 unicodeFFFE Unicode (Big-Endian)
+                    enc = Encoding.GetEncoding(1201);
+                else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+                    // 1200 utf-16 Unicode
+                    enc = Encoding.GetEncoding(1200);
+                else if (ValidateUTF8WithoutBOM(srcFile))
+                    enc = new UTF8Encoding(false);
+                return enc;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static bool ValidateUTF8WithoutBOM(string filePath)
+        {
+            //ansi로 인코딩 된 파일을 utf-8을 사용하여 읽으면 각 기본 언어마다 특수문자가 포함되는 것같다.
+            //euc-kr 에서 utf-8로 읽으면 "� = (char)0xfffd)"가 생기는 것 같다. 
+            using (FileStream fs = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                if (fs.Length < 1024 * 1000)
+                {
+                    if (sr.ReadToEnd().Contains((char)0xfffd))
+                        return false;
+                    else
+                        return true;
+                }
+
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (s.Contains((char)0xfffd))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
